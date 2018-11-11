d64a9055067dd9fb40c29ff747be3dcb1c1b4129
zydplatform <kigandaivan32@gmail.com>
2018-11-11 15:47:28 +0300

    setting configurations

---
+++ app/config.py
@@ -0,0 +1,31 @@
+# configuration  for all application stages:
+# development,testing,staging,production
+
+class Config(object):
+    DEBUG = False
+
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    # for testing this we shall require a separate database
+    TESTING = True
+
+    DEBUG = True
+
+class StagingConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+    TESTING = False
+
+app_config = {
+    'development':DevelopmentConfig,
+    'testing':TestingConfig,
+    'staging':StagingConfig,
+    'production':ProductionConfig
+}
